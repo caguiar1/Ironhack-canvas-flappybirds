a9e06b767c38e9852c766a3badbfaec07a3baa3b
Christopher Aguiar <epicchris17@gmail.com>
2020-01-29 12:55:12 -0500

    aaaaaaaaaaaaaaaa

--- starter_code/script.js
+++ starter_code/script.js
@@ -6,14 +6,41 @@ window.onload = function() {
     startGame();
   };
   
+  let canvas = document.getElementById('canvas');
+  let ctx = canvas.getContext('2d');
   
-  function startGame() {
+  
 
+
+  // ------------------------ STARTS THE GAME ------------------------ //
+
+
+  function startGame() {
+    
+class Flappy {
+    constructor() {
+      this.x = 200;
+      this.y = 200;
+      this.width = 50;
+      this.height = 50;
+      this.img = new Image();
+      this.img.src = "./images/flappy.png";
+    }
+    draw() {
+      this.y += 3;
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+ 
+  }
+  let flappy = new Flappy();
+
+  flappy.draw();
+    
     
     
     let faby = {
-      x: 25,
-      y: 25,
+      x: 250,
+      y: 205,
       width: 10,
       height: 10,
       speedX: 10,
@@ -21,24 +48,74 @@ window.onload = function() {
       gravity: 0.05,
       gravitySpeed: 0.5,
       update: () => {
+
         
       },
       newPos: () => {
         update();
       }
+
       
     }
+
     
     
 
-    function draw(faby) {
-      let img = new Image();
-      img.onload = function() {
-        ctx.drawImage(img, faby.x, faby.y, 50, 50);
-      }
-      img.src = "images/flappy.png";
+    // function draw(faby) {
+    //   let img = new Image();
+    //   img.onload = function() { 
+    //      ctx.drawImage(img, faby.x, faby.y, 500, 350); 
+    //   }
+    //   img.src = "images/flappy.png";
+    // }
+    
+    
+    
+    // ------------------------ SCROLLING IMAGE ------------------------ //
+
+    
+    let img = new Image();
+    img.src = 'images/bg.png';
+    
+    
+    let backgroundImage = {
+      img: img,
+      x: 0,
+      speed: -1,
+    
+      move: function() {
+        this.x += this.speed;
+        this.x %= canvas.width;
+      },
+      
+      draw: function() {
+        ctx.drawImage(this.img, this.x, 0);
+        if (this.speed < 0) {
+          ctx.drawImage(this.img, this.x + canvas.width, 0);
+        } else {
+          ctx.drawImage(this.img, this.x - this.img.width, 0);
+        }
+      },
+    };
+    
+    function updateCanvas() {
+      backgroundImage.move();
+      
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      backgroundImage.draw();
+    
+      requestAnimationFrame(updateCanvas);
+      
+      // draw(faby)
     }
     
+    // start calling updateCanvas once the image is loaded
+    img.onload = updateCanvas;
+
+
+
+    
+    //------------------------ GRAVITY ------------------------ //
     
     document.onkeydown = function(e) {
       switch (e.keyCode) {
@@ -56,60 +133,16 @@ window.onload = function() {
         faby.gravity = 0.05
         console.log('RESET GRAVITY')
       }
-
+    
       // updateCanvas();
+
+
     }
-
-
-
-
-    let img = new Image();
-    img.src = 'images/bg.png';
-    
-    let canvas = document.getElementById('gametime');
-    let ctx = canvas.getContext('2d');
-    
-    let backgroundImage = {
-      img: img,
-      x: 0,
-      speed: -1,
-    
-      move: function() {
-        this.x += this.speed;
-        this.x %= canvas.width;
-      },
-    
-      draw: function() {
-        ctx.drawImage(this.img, this.x, 0);
-        if (this.speed < 0) {
-          ctx.drawImage(this.img, this.x + canvas.width, 0);
-        } else {
-          ctx.drawImage(this.img, this.x - this.img.width, 0);
-        }
-      },
-    };
-    
-    function updateCanvas() {
-      backgroundImage.move();
-    
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      backgroundImage.draw();
-    
-      requestAnimationFrame(updateCanvas);
-
-      draw(faby)
-    }
-    
-    // start calling updateCanvas once the image is loaded
-    img.onload = updateCanvas;
-
   }
-
-
-
-
-
-
-
-
+  
+  
+  
+  
+  
+  
 };
